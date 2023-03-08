e67f61b7628615eb5bb4cae25c895ec58ca9f0d1
imisspotato <116682396+imisspotato@users.noreply.github.com>
2023-03-08 15:07:15 -0500

    update language

--- src/default.ts
+++ src/default.ts
@@ -11,3 +11,4 @@ export const defaultMessage = `
 - Supported by [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) and [Vercel](http://vercel.com/),
 - Modified by [ourongxing](https://github.com/ourongxing) based on [Diu](https://github.com/ddiu8081/chatgpt-demo). See [source code](https://github.com/ourongxing/chatgpt-vercel) and feel free to deploy it yourself.
 - **Shift+Enter**  is used to create a new line. Begin your search for Prompt presets by typing **"/"** or **" "** at the beginning. Scroll to the bottom of the input box and click to access the presets.
+ `
